41c6a2a46de3d8d7a19bb503a8634755027c916b
Jan Erik Breimo <jebreimo@gmail.com>
2023-04-11 00:18:51 +0200

    Add missing include file.

--- include/Xyz/SphericalPoint.hpp
+++ include/Xyz/SphericalPoint.hpp
@@ -6,6 +6,7 @@
 // License text is included with the source distribution.
 //****************************************************************************
 #pragma once
+#include "Utilities.hpp"
 #include "Vector.hpp"
 
 namespace Xyz
