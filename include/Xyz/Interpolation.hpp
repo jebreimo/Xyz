2a5008d2af4ac8711ae3b6c54fb49ad263eddcf0
Jan Erik Breimo <jebreimo@gmail.com>
2025-05-11 11:33:20 +0200

    Enhance bilinear interpolation function with additional overload and improved parameter order

--- include/Xyz/Interpolation.hpp
+++ include/Xyz/Interpolation.hpp
@@ -10,17 +10,48 @@
 
 namespace Xyz
 {
-    template <typename T>
+    /**
+     * @brief Performs bilinear interpolation of a value in a rectangle.
+     *
+     * @param q The values at the corners of the rectangle. The order is
+     *  (0, 0), (1, 0), (0, 1), (1, 1).
+     * @param p The point to interpolate.
+     * @param min The minimum corner of the rectangle.
+     * @param max The maximum corner of the rectangle.
+     * @return The interpolated value.
+     */
+    template <typename T, typename U>
     T bilinear(const Vector<T, 4>& q,
-               const Vector<std::type_identity_t<T>, 2>& min,
-               const Vector<std::type_identity_t<T>, 2>& max,
-               const Vector<std::type_identity_t<T>, 2>& p)
+               const Vector<U, 2>& p,
+               const Vector<std::type_identity_t<U>, 2>& min,
+               const Vector<std::type_identity_t<U>, 2>& max)
     {
         auto [dx12, dy12] = max - min;
         auto [dx1, dy1] = p - min;
         auto [dx2, dy2] = max - p;
-        return (q[0] * dx2 * dy2 + q[1] * dx1 * dy2
-                + q[2] * dx2 * dy1 + q[3] * dx1 * dy1)
-            / (dx12 * dy12);
+
+        return static_cast<T>(
+            (
+                q[0] * dx2 * dy2
+                + q[1] * dx1 * dy2
+                + q[2] * dx2 * dy1
+                + q[3] * dx1 * dy1
+            ) / (dx12 * dy12));
+    }
+
+    /**
+     * @brief Performs bilinear interpolation of a value in a rectangle.
+     *
+     * @param q The values at the corners of the rectangle. The order is
+     *  (0, 0), (1, 0), (0, 1), (1, 1).
+     * @param p The point to interpolate. Coordinates are in the range
+     *  [0, 1].
+     * @return The interpolated value.
+     */
+    template <typename T, typename U>
+    T bilinear(const Vector<T, 4>& q,
+               const Vector<U, 2>& p)
+    {
+        return bilinear(q, p, {0, 0}, {1, 1});
     }
 }
--- tests/CatchXyzTest/test_Interpolation.cpp
+++ tests/CatchXyzTest/test_Interpolation.cpp
@@ -9,7 +9,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
-TEST_CASE("bilinear interpolation")
+TEST_CASE("bilinear interpolation with min and max")
 {
     using namespace Xyz;
     using Catch::Matchers::WithinAbs;
@@ -17,11 +17,28 @@ TEST_CASE("bilinear interpolation")
     auto p1 = Vector2F(10, 10);
     auto p2 = Vector2F(15, 15);
     Vector4F q(1, 2, 3, 4);
+    using V = Vector2F;
 
-    REQUIRE_THAT(bilinear(q, p1, p2, {10, 10}), WithinAbs(1.0f, 0.00001f));
-    REQUIRE_THAT(bilinear(q, p1, p2, {12.5, 10}), WithinAbs(1.5f, 0.00001f));
-    REQUIRE_THAT(bilinear(q, p1, p2, {10, 12.5}), WithinAbs(2.0f, 0.00001f));
-    REQUIRE_THAT(bilinear(q, p1, p2, {12.5f, 12.5f}), WithinAbs(2.5f, 0.00001f));
-    REQUIRE_THAT(bilinear(q, p1, p2, {15, 12.5}), WithinAbs(3.0f, 0.00001f));
-    REQUIRE_THAT(bilinear(q, p1, p2, {12.5, 15}), WithinAbs(3.5f, 0.00001f));
+    REQUIRE_THAT(bilinear(q, V{10, 10}, p1, p2), WithinAbs(1.0f, 0.00001f));
+    REQUIRE_THAT(bilinear(q, V{12.5, 10}, p1, p2), WithinAbs(1.5f, 0.00001f));
+    REQUIRE_THAT(bilinear(q, V{10, 12.5}, p1, p2), WithinAbs(2.0f, 0.00001f));
+    REQUIRE_THAT(bilinear(q, V{12.5f, 12.5f}, p1, p2), WithinAbs(2.5f, 0.00001f));
+    REQUIRE_THAT(bilinear(q, V{15, 12.5}, p1, p2), WithinAbs(3.0f, 0.00001f));
+    REQUIRE_THAT(bilinear(q, V{12.5, 15}, p1, p2), WithinAbs(3.5f, 0.00001f));
+}
+
+TEST_CASE("bilinear interpolation with default min and max")
+{
+    using namespace Xyz;
+    using Catch::Matchers::WithinAbs;
+
+    Vector4F q(1, 2, 3, 4);
+    using V = Vector2F;
+
+    REQUIRE_THAT(bilinear(q, V{0, 0}), WithinAbs(1.0f, 0.00001f));
+    REQUIRE_THAT(bilinear(q, V{0.5, 0}), WithinAbs(1.5f, 0.00001f));
+    REQUIRE_THAT(bilinear(q, V{0, 0.5}), WithinAbs(2.0f, 0.00001f));
+    REQUIRE_THAT(bilinear(q, V{0.5f, 0.5f}), WithinAbs(2.5f, 0.00001f));
+    REQUIRE_THAT(bilinear(q, V{1, 0.5}), WithinAbs(3.0f, 0.00001f));
+    REQUIRE_THAT(bilinear(q, V{0.5, 1}), WithinAbs(3.5f, 0.00001f));
 }
