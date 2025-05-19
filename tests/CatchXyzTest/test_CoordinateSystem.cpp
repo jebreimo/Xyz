301dc5385b113ccba87dcd004d29d089a537d885
Jan Erik Breimo <jebreimo@gmail.com>
2025-05-19 23:27:03 +0200

    Move CoordinateSystem test from Ytest to Catch2.

--- tests/CatchXyzTest/test_CoordinateSystem.cpp
+++ tests/CatchXyzTest/test_CoordinateSystem.cpp
@@ -5,30 +5,28 @@
 // This file is distributed under the Zero-Clause BSD License.
 // License text is included with the source distribution.
 //****************************************************************************
-#include <Ytest/Ytest.hpp>
-#include <Xyz/Vector.hpp>
 #include <Xyz/CoordinateSystem.hpp>
+#include <Xyz/Vector.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 namespace
 {
-    void test_roundtrip()
+    TEST_CASE("CoordinateSystem rotated XY", "[CoordinateSystem]")
     {
         Xyz::CoordinateSystem<double> sys({1, 1, 0}, {1, 1, 0},
                                           {-1, 1, 0}, {0, 0, 2});
         Xyz::Vector3D p0 = {2, 0, 0.5};
         auto p1 = sys.from_world_pos(p0);
         auto p2 = sys.to_world_pos(p1);
-        Y_EQUAL(p0, p2);
+        REQUIRE(p0 == p2);
     }
 
-    void test_something()
+    TEST_CASE("CoordinateSystem YZX", "[CoordinateSystem]")
     {
         Xyz::CoordinateSystem<double> sys({0, 0, 0}, {0, 1, 0},
                                           {0, 0, 1}, {1, 0, 0});
         Xyz::Vector3D p1 = {-1, -1, 1};
         auto p2 = sys.to_world_pos(p1);
-        Y_EQUAL(p2, Xyz::make_vector3(1, -1, -1));
+        REQUIRE(p2 == Xyz::make_vector3(1, -1, -1));
     }
-
-    Y_TEST(test_roundtrip, test_something);
 }
