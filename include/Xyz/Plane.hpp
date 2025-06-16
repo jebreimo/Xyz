9ebe698e19483f2fe5899ec1f76932450aedd8eb
Jan Erik Breimo <jebreimo@gmail.com>
2025-06-16 21:42:00 +0200

    Add get_intersection function for Plane class; implement intersection logic and corresponding tests

--- include/Xyz/Plane.hpp
+++ include/Xyz/Plane.hpp
@@ -7,6 +7,8 @@
 //****************************************************************************
 #pragma once
 
+#include "Approx.hpp"
+#include "Line.hpp"
 #include "Matrix.hpp"
 
 namespace Xyz
@@ -44,5 +46,69 @@ namespace Xyz
         result[{3, 3}] = 1;
 
         return result;
+    }
+
+    template <typename T, typename FloatT = FloatType_t<T>>
+    [[nodiscard]]
+    std::optional<Line<FloatT, 3>>
+    get_intersection(const Plane<T, 3>& p1, const Plane<T, 3>& p2,
+                     std::type_identity_t<T> margin = Margin<T>::DEFAULT)
+    {
+        auto vec = cross(p1.normal, p2.normal);
+        if (are_equal(vec, Vector<T, 3>(), margin))
+            return std::nullopt; // Planes are parallel
+
+        Vector<T, 3> point;
+        auto s = dot(p1.origin, p1.normal);
+        auto t = dot(p2.origin, p2.normal);
+        if (std::abs(p1.normal[0]) > margin)
+        {
+            FloatT dividend = p1.normal[0] * t - p2.normal[0] * s;
+            if (std::abs(vec[2]) > margin)
+            {
+                point[2] = 0;
+                point[1] = dividend / vec[2];
+                point[0] = s - p1.normal[1] * point[1] / p1.normal[0];
+            }
+            else
+            {
+                point[1] = 0;
+                point[2] = dividend / -vec[1];
+                point[0] = s - p1.normal[2] * point[2] / p1.normal[0];
+            }
+        }
+        else if (std::abs(p1.normal[1]) > margin)
+        {
+            FloatT dividend = p1.normal[1] * t - p2.normal[1] * s;
+            if (std::abs(vec[2]) > margin)
+            {
+                point[2] = 0;
+                point[0] = dividend / -vec[2];
+                point[1] = s - p1.normal[0] * point[0] / p1.normal[1];
+            }
+            else
+            {
+                point[0] = 0;
+                point[2] = dividend / vec[0];
+                point[1] = s - p1.normal[2] * point[2] / p1.normal[1];
+            }
+        }
+        else
+        {
+            FloatT dividend = p1.normal[2] * t - p2.normal[2] * s;
+            if (std::abs(vec[1]) > margin)
+            {
+                point[1] = 0;
+                point[0] = dividend / vec[1];
+                point[2] = s - p1.normal[0] * point[0] / p1.normal[2];
+            }
+            else
+            {
+                point[0] = 0;
+                point[1] = dividend / -vec[0];
+                point[2] = s - p1.normal[1] * point[1] / p1.normal[2];
+            }
+        }
+        return {Line(point, vec)};
     }
 }
--- tests/CatchXyzTest/test_Plane.cpp
+++ tests/CatchXyzTest/test_Plane.cpp
@@ -16,9 +16,40 @@ TEST_CASE("Plane: projection matrix on")
     using namespace Xyz;
     using Catch::Matchers::WithinAbs;
 
-    Plane<double, 3> plane{{2, -1, 0}, {0, 1, 1}};
-    auto m = make_projection_matrix(plane);
+    constexpr Plane<double, 3> plane{{2, -1, 0}, {0, 1, 1}};
+    const auto m = make_projection_matrix(plane);
     using V = Vector4D;
     CHECK(are_equal(m * V(2, -1, 0, 1), V(2, -1, 0, 1), 1e-10));
     CHECK(are_equal(m * V(5, 2, 3, 1), V(5, -1, 0, 1), 1e-10));
+}
+
+TEST_CASE("Plane: intersection with diagonal plane")
+{
+    using V = Xyz::Vector3F;
+    using Plane = Xyz::Plane<float, 3>;
+    Plane plane{{-1.5, 2, 5.5}, {-1, -1, 1}};
+    SECTION("Intersection with x = 0 plane")
+    {
+        constexpr Plane x_is_0 {{0, 0, 0}, {1, 0, 0}};
+        auto line = Xyz::get_intersection(x_is_0, plane);
+        REQUIRE(line);
+        REQUIRE(are_equal(line->point, V(0, -5, 0)));
+        REQUIRE(are_equal(line->vector, V(0, -1, -1)));
+    }
+    SECTION("Intersection with y = 0 plane")
+    {
+        constexpr Plane y_is_0 {{0, 0, 0}, {0, 1, 0}};
+        auto line = Xyz::get_intersection(y_is_0, plane);
+        REQUIRE(line);
+        REQUIRE(are_equal(line->point, V(-5, 0, 0)));
+        REQUIRE(are_equal(line->vector, V(1, 0, 1)));
+    }
+    SECTION("Intersection with z = 0 plane")
+    {
+        constexpr Plane z_is_0 {{0, 0, 0}, {0, 0, 1}};
+        auto line = Xyz::get_intersection(z_is_0, plane);
+        REQUIRE(line);
+        REQUIRE(are_equal(line->point, V(-5, 0, 0)));
+        REQUIRE(are_equal(line->vector, V(1, -1, 0)));
+    }
 }
