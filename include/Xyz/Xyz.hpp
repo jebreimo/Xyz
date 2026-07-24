06d5b169f01d8cbf3caf5208a3573a82f52a820d
Jan Erik Breimo <jebreimo@gmail.com>
2026-07-24 22:50:36 +0200

    Include AABB.hpp in Xyz.hpp.

--- include/Xyz/Xyz.hpp
+++ include/Xyz/Xyz.hpp
@@ -7,6 +7,7 @@
 //****************************************************************************
 #pragma once
 
+#include "AABB.hpp"
 #include "ComplexApprox.hpp"
 #include "Interpolation.hpp"
 #include "Line.hpp"
