f658003e97893bd01bb851a256a5b60604f89415
Jan Erik Breimo <jebreimo@gmail.com>
2026-08-12 15:10:09 +0200

    Fix Ubuntu build.

--- include/Xyz/Mesh/MeshBuilder.hpp
+++ include/Xyz/Mesh/MeshBuilder.hpp
@@ -6,6 +6,7 @@
 // License text is included with the source distribution.
 //****************************************************************************
 #pragma once
+#include <cstdint>
 #include <optional>
 
 #include "MeshAttributeBuilder.hpp"
